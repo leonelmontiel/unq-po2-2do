f3ae1c3b8622d264a4de8b26cc73df3e12fd864b
leonelmontiel <leonel.montiel@outlook.com>
2021-10-09 21:40:15 -0300

    TP8 PokerStatus con Strings

--- src/ar/edu/unq/po2/tp8/PokerStatus.java
+++ src/ar/edu/unq/po2/tp8/PokerStatus.java
@@ -5,22 +5,66 @@ import java.util.List;
 
 public class PokerStatus {
 
+	/* VERIFICAR VIEJO
 	public boolean verificar(String carta1, String carta2, String carta3, String carta4, String carta5) {
 		List<String> cartas = Arrays.asList(carta1, carta2, carta3, carta4, carta5);
 		
 		//this.verificarQueNoHayaIdenticas(cartas);
 		
-		//devuelve una lista con las cartas únicas sin sus duplicadas, si hay una carta que no se duplica la devuelve también
-		List<String> sinDuplicadas = this.cartasSoloConValores(cartas).stream().distinct().toList();
+		return this.hayPoquer(cartas);
+	}*/
+	
+	public String verificar(String carta1, String carta2, String carta3, String carta4, String carta5) {	
+		List<String> cartas = Arrays.asList(carta1, carta2, carta3, carta4, carta5);
 		
-		// se compara con 2 porque 3 elementos son los que se repiten, y se debe tener en cuenta el cuarto que es el original
-		boolean hayPoker = sinDuplicadas.size() == 2;
-		return hayPoker;
+		//this.verificarQueNoHayaIdenticas(cartas);
+		
+		if(this.hayPoquer(cartas)) {return "Póquer";} 
+		if(this.hayTrio(cartas)) {return "Trío";}
+		if(this.hayColor(cartas)) {return "Color";}
+		return "Nada";
 	}
 
+	///////////////////////
 	public List<String> cartasSoloConValores(List<String> cartas) {
 		List<String> valores = cartas.stream().map(c -> c.replaceFirst(".$","")).toList();		
 	    return valores;
+	}
+	
+	public List<String> cartasSoloConPalos(List<String> cartas) {
+		List<String> valores = cartas.stream().map(c -> c.substring(c.length() - 1)).toList();		
+	    return valores;
+	}
+	
+	///////////////////////		
+	// Evalua cada carta de la lista de String, y si su valor se repite cuatro veces en la lista entonces hay póquer
+	public boolean hayPoquer(List<String> cartas) {
+		return cantCadenaEquivaleA(this.cartasSoloConValores(cartas), 4);
+	}
+	
+	// hace lo mismo que en "hayPoquer" pero evalúa si hay 3 valores iguales
+	public boolean hayTrio(List<String> cartas) {
+		return cantCadenaEquivaleA(this.cartasSoloConValores(cartas), 3);
+	}
+	
+	// convierte la lista de String en una nueva lista con la representación de los palos, y a partir de ella itera y cuenta si el palo de la primera se
+	// repite 5 veces en la lista de palos generada
+	public boolean hayColor(List<String> cartas) {
+		return cantCadenaEquivaleA(this.cartasSoloConPalos(cartas), 5);
+	}
+
+	/////////////////////
+	public boolean cantCadenaEquivaleA(List<String> cartas, int valor) {
+		return cartas.stream().anyMatch(carta -> contarRepeticiones(carta, cartas) == valor);
+	}
+	
+	// cuenta el total de cadenas que son iguales a otra dada por parámetro
+	public long contarRepeticiones(String cadena, List<String> cadenas){
+		
+	    long rep  = cadenas.stream()
+	                      .filter(c -> c.equals(cadena))
+	                      .count();
+	    return rep;
 	}
 
 }
--- test/ar/edu/unq/po2/tp8/PoquerStatusTest.java
+++ test/ar/edu/unq/po2/tp8/PoquerStatusTest.java
@@ -28,27 +28,29 @@ class PoquerStatusTest {
 		carta5 = "QD";
 	}
 
+	/*
 	@Test
-	void testEsPoker() {
+	void testHayPokerV1() {
 		//Excercise
-		boolean esPoker = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		boolean hayPoker = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
 		
 		//verify
-		assertTrue(esPoker);
+		assertTrue(hayPoker);
 	}
 	
 	@Test
-	void testNoEsPoker() {
+	void testNoHayPokerV1() {
 		carta4 = "5T";
 		//Excercise
-		boolean esPoker = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		boolean hayPoker = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
 		
 		//verify
-		assertFalse(esPoker);
-	}
+		assertFalse(hayPoker);
+	}*/
 	
 	@Test
 	void testCartasSoloConSusValores() {
+		//SetUp
 		List<String> cartas = Arrays.asList(carta1, carta2, carta3, carta4, carta5);
 		List<String> valoresDeseados = Arrays.asList("2", "2", "2", "2", "Q");
 		
@@ -59,6 +61,67 @@ class PoquerStatusTest {
 		assertEquals(valoresDeseados, valoresObtenidos);
 	}
 	
+	@Test
+	void testHayPokerV2() {
+		//Excercise
+		String cadenaObtenida = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		//Verify
+		assertTrue("Póquer".equals(cadenaObtenida));
+	}
 	
+	@Test
+	void testNoHayPokerV2() {
+		//SetUp
+		carta1 = "QT";
+		//Excercise
+		String cadenaObtenida = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		assertFalse("Póquer".equals(cadenaObtenida));
+	}
+	
+	@Test
+	void testHayTrio() {
+		//Setup
+		carta1 = "KD";
+		//Excercise
+		String cadenaObtenida = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		//Verify
+		assertTrue("Trío".equals(cadenaObtenida));
+	}
+	
+	@Test
+	void testNoHayTrio() {
+		//Excercise
+		String cadenaObtenida = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		//Verify
+		assertFalse("Trío".equals(cadenaObtenida));
+	}
+	
+	@Test
+	void testHayColor() {
+		//SetUp
+		carta1 = "KD";	carta2 = "2D";	carta3 = "JD";	carta4 = "10D";
+		//Excercise
+		String cadenaObtenida = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		//Verify
+		assertTrue("Color".equals(cadenaObtenida));
+	}
+	
+	@Test
+	void testNoHayColor() {
+		//Excercise
+		String cadenaObtenida = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		//Verify
+		assertFalse("Color".equals(cadenaObtenida));
+	}
+	
+	@Test
+	void testNoHayNada() {
+		//SetUp
+		carta1 = "KD";	carta2 = "2D";	carta3 = "JD";	carta4 = "10D"; carta5 = "AP";
+		//Excercise
+		String cadenaObtenida = pokerStatus.verificar(carta1, carta2, carta3, carta4, carta5);
+		//Verify
+		assertTrue("Nada".equals(cadenaObtenida));
+	}
 
 }
