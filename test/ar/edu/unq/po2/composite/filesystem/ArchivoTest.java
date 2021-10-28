484ef54bcb6a808f4bc83bcec395b4df562aa321
leonelmontiel <leonel.montiel@outlook.com>
2021-10-28 18:13:55 -0300

    test para directorio

--- test/ar/edu/unq/po2/composite/filesystem/ArchivoTest.java
+++ test/ar/edu/unq/po2/composite/filesystem/ArchivoTest.java
@@ -1,27 +1,59 @@
 package ar.edu.unq.po2.composite.filesystem;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ArchivoTest extends ElementoFileSystemTest{
+class ArchivoTest {
+	
+	private IFileSystem archivo; //SUT
 
 	@BeforeEach
 	void setUp() throws Exception {
-		this.elemento = new Archivo("Elemento Genérico", 50);
+		this.archivo = new Archivo("Snow.mp3", 50);
+	}
+	
+	@Test
+	void testNombreEsSnow_mp3() {
+		//Excercise
+		String resultadoObtenido = this.archivo.getNombre();
+		//Verify
+		assertEquals("Snow.mp3", resultadoObtenido);
+	}
+
+	@Test
+	void testTamañoTotalEs50Bytes() {
+		// Excercise
+		int resultadoObtenido = this.archivo.totalSize();
+		//Verify
+		assertEquals(50, resultadoObtenido);
 	}
 	
 	@Test
 	void estElementoMásAntiguoEsÉlMismo() {
-		assertEquals(this.elemento, this.elemento.oldestElement());
+		assertEquals(this.archivo, this.archivo.oldestElement());
 	}
 	
 	@Test
 	void testElementoMásNuevoEsÉlMismo() {
-		assertEquals(this.elemento, this.elemento.lastModified());
+		assertEquals(this.archivo, this.archivo.lastModified());
 	}
 	
+	@Test
+	void testPrintStructure() {
+		this.archivo.printStructure();
+	}
 	
+	@Test
+	void testFechaültimaModificación() {
+		// Excercise
+		LocalDate fechaEsperada = LocalDate.now();
+		LocalDate fechaObtenida = ((Archivo) this.archivo).getUltimaModificacion();
+		//Verify
+		assertEquals(fechaEsperada, fechaObtenida);
+	}
 
 }
---
+++ test/ar/edu/unq/po2/composite/filesystem/DirectorioTest.java
@@ -0,0 +1,81 @@
+package ar.edu.unq.po2.composite.filesystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DirectorioTest {
+
+	private IFileSystem directorio; //SUT
+	private IFileSystem archivo = mock(Archivo.class);
+	private IFileSystem directorioAux = mock(Directorio.class);
+
+	@BeforeEach
+	void setUp() throws Exception {
+		this.directorio = new Directorio("RHCP");
+		
+		//Config mocks
+		when(this.archivo.totalSize()).thenReturn(50);
+		when(this.directorioAux.totalSize()).thenReturn(50);
+	}
+
+	@Test
+	void testNombreEsRHCP() {
+		//Excercise
+		String resultadoObtenido = this.directorio.getNombre();
+		//Verify
+		assertEquals("RHCP", resultadoObtenido);
+	}
+	
+	@Test
+	void testFechaDeCreación() {
+		//Excercise
+		LocalDate fechaEsperada = LocalDate.now();
+		LocalDate fechaObtenida = ((Directorio) this.directorio).getFechaCreacion();
+		//Verify
+		assertEquals(fechaEsperada, fechaObtenida);
+		
+	}
+	
+	@Test
+	void testGetContenidoVacío() {
+		//Excercise
+		List<IFileSystem> listaObtenida = ((Directorio) this.directorio).getContenido();
+		//Verify
+		assertTrue(listaObtenida.isEmpty());
+	}
+	
+	@Test
+	void testGetContenidoNoEsVacío() {
+		//Excercise
+		((Directorio) this.directorio).agregarElemento(archivo);
+		List<IFileSystem> listaObtenida = ((Directorio) this.directorio).getContenido();
+		//Verify
+		assertFalse(listaObtenida.isEmpty());
+	}
+	
+	@Test
+	void testContieneAlArchivo() {
+		//Excercise
+		((Directorio) this.directorio).agregarElemento(archivo);
+		boolean contieneAlArchivo = ((Directorio) this.directorio).contains(archivo);
+		//Verify
+		assertTrue(contieneAlArchivo);
+	}
+	/*
+	@Test
+	void testTamañoTotalEs100BytesPorContenerArchivoYDirectorioConArchivo() {
+		// Excersice
+		int resultadoObtenido = this.directorio.totalSize();
+		//Verify
+		assertEquals(100, resultadoObtenido);
+		verify(this.archivo, times(2)).totalSize();
+		verify(this.directorioAux, times(1)).totalSize();
+	}*/
+
+}
