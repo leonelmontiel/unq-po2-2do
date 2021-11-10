e03cfffd4b7d33cc78e2661c7e129cef1ede7411
leonelmontiel <leonel.montiel@outlook.com>
2021-11-10 17:23:23 -0300

    subo métodos de hoja a la súperclase

--- src/ar/edu/unq/po2/composite/shapeShifter/Hoja.java
+++ src/ar/edu/unq/po2/composite/shapeShifter/Hoja.java
@@ -11,18 +11,6 @@ public class Hoja extends ShapeShifter {
 		this.value = value;
 	}
 
-	@Override
-	public List<IShapeShifter> getElementos() {
-		//se devuelve a sí misma en una lista porque esto es útil para el método flat() en nodo
-		return Arrays.asList(this);
-	}
-	
-	@Override
-	public Boolean contiene(IShapeShifter elemento) {
-		System.out.println("Una hoja no puede contener elementos de IShapeShifter");
-		return false;
-	}
-
 	public int getValue() {
 		return this.value;
 	}
@@ -30,19 +18,6 @@ public class Hoja extends ShapeShifter {
 	@Override
 	public List<Integer> values() {
 		return Arrays.asList(this.getValue());
-	}
-
-	@Override
-	public IShapeShifter flat() {
-		/* Si el IShapeShifter posee una profundidad máxima >= 1, entonces retorna un IShapeShifter de profundidad maxima 1 con todos los 
-		IShapeShifter de profundidad 0 contenidos. En cualquier otro caso, retorna el mismo IShapeShifter.*/
-		return this;
-	}
-
-	@Override
-	public int deepest() {
-		// 0 es la profundidad base de una hoja
-		return 0;
 	}
 
 }
--- src/ar/edu/unq/po2/composite/shapeShifter/ShapeShifter.java
+++ src/ar/edu/unq/po2/composite/shapeShifter/ShapeShifter.java
@@ -1,4 +1,5 @@
 package ar.edu.unq.po2.composite.shapeShifter;
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class ShapeShifter implements IShapeShifter{	
@@ -9,8 +10,29 @@ public abstract class ShapeShifter implements IShapeShifter{
 		return composicion;
 	}
 	
-	public abstract Boolean contiene(IShapeShifter elemento);
+	/* IMPLEMENTO LOS COMPORTAMIENTOS DE LA HOJA COMO SI FUERAN PREDETERMINADOS, DE ESTA MANERA NO ROMPO CON EL PRINCIPIO DE 
+	 * DISEÑO DE JERARQUÍAS DE CLASES, QUE DICE QUE UNA CLASE SOLO DEBERÍA DEFINIR OPERACIONES QUE TIENEN SENTIDO EN SUS SUBCLASES
+	 */
 	
-	public abstract List<IShapeShifter> getElementos();
+	public List<IShapeShifter> getElementos() {
+		//se devuelve a sí misma en una lista porque esto es útil para el método flat() en nodo
+		return Arrays.asList(this);
+	}
+	public Boolean contiene(IShapeShifter elemento) {
+		return false;
+	}
+	
+	@Override
+	public IShapeShifter flat() {
+		/* Si el IShapeShifter posee una profundidad máxima >= 1, entonces retorna un IShapeShifter de profundidad maxima 1 con todos los 
+		IShapeShifter de profundidad 0 contenidos. En cualquier otro caso, retorna el mismo IShapeShifter.*/
+		return this;
+	}
+	
+	@Override
+	public int deepest() {
+		// 0 es la profundidad base de una hoja
+		return 0;
+	}
 
 }
