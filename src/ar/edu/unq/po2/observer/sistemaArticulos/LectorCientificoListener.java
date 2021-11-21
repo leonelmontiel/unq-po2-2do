04d274843d5d6a17632851d3be0a4bbf5ca69028
leonelmontiel <leonel.montiel@outlook.com>
2021-11-21 16:23:27 -0300

    quito getIntereses

--- src/ar/edu/unq/po2/observer/sistemaArticulos/LectorCientificoListener.java
+++ src/ar/edu/unq/po2/observer/sistemaArticulos/LectorCientificoListener.java
@@ -3,11 +3,13 @@ package ar.edu.unq.po2.observer.sistemaArticulos;
 import java.util.Set;
 
 public interface LectorCientificoListener {
+	
+	void suscribirseA(SistemaRyPB sistema);
+	
+	void desuscribirseA(SistemaRyPB sistema);
 
 	void recibirNuevoArticulo(SistemaRyPB sistema, ArticuloCientifico articulo);
 
 	Boolean estaInteresadoEnAlgunaPalabraDe(Set<String> palabrasClaves);
-
-	Set<String> getIntereses();
 
 }
